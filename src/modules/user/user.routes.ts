f204505dc12e31bebb8be9455ff3e4c851e667c3
@dev.arthur <contato.arthurreis222@gmail.com>
2024-11-03 17:34:29 -0300

    feat: insert authenticated middleware

--- src/modules/user/user.routes.ts
+++ src/modules/user/user.routes.ts
@@ -1,5 +1,6 @@
 import { Router } from "express";
 import { UserController } from "./user.controller";
+import { AuthenticatedMiddleware } from "../../http/middlewares/authenticated.middleware";
 
 const userRoutes = Router();
 
@@ -7,5 +8,6 @@ export const userController = new UserController();
 
 userRoutes.post("/", userController.create);
 userRoutes.post("/session", userController.session);
+userRoutes.get("/", AuthenticatedMiddleware, userController.details);
 
 export { userRoutes };
